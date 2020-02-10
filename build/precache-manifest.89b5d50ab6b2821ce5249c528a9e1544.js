a503f2ad96ce423cafe3d81eefe607fd3e1da63e
pauloJimenez <38992516+pauloJimenez@users.noreply.github.com>
2020-02-09 23:27:46 -0300

    Add files via upload

---
+++ build/precache-manifest.89b5d50ab6b2821ce5249c528a9e1544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcaea160e54bc57319b2c65300da1eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da0e61cf093c2efbd21f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e8e3ace57248840b032a",
+    "url": "/static/js/2.88cd7d9e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.88cd7d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0e61cf093c2efbd21f",
+    "url": "/static/js/main.eae6fdaf.chunk.js"
+  },
+  {
+    "revision": "378d6087cf4fd0c5cb92",
+    "url": "/static/js/runtime-main.fbb1b6ef.js"
+  }
+]);
